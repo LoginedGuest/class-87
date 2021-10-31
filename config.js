048d60a588fee260aa0662993a6a717813ba25c8
LoginedGuest <90995728+LoginedGuest@users.noreply.github.com>
2021-10-31 12:49:58 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBVaxTVpxOArgeldrfrbdeacCsZRbrsl4I",
+    authDomain: "storytelling-2cfdc.firebaseapp.com",
+    databaseURL: "https://storytelling-2cfdc-default-rtdb.firebaseio.com",
+    projectId: "storytelling-2cfdc",
+    storageBucket: "storytelling-2cfdc.appspot.com",
+    messagingSenderId: "989403944179",
+    appId: "1:989403944179:web:7e9c8d10b0ae34c08a1386"
+};
